7b0675218f6eecfad8e76b72fa73f92687811634
Your Name <you@example.com>
2024-03-19 11:31:39 +0100

    WILDERS TEST

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+const log = console.log
+log(chalk.blue.bgRed.bold('Alexis'));
+log(chalk.cyan.bgBlackBright.dim('Audrey'))
+log(chalk.whiteBright.bgBlackBright.overline.black('Arnauld'))
+log(chalk.rgb(255, 136, 0).bold('Elias'))
